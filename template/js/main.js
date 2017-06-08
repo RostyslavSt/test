8f678afe6ab519128fcf39c942cff2b1948d5bc1
rost <rost@ukr.net>
2017-06-08 14:15:43 +0300

    added img dashboard

--- template/js/main.js
+++ template/js/main.js
@@ -1,7 +1,30 @@
 let dashboard = document.querySelector('.main-dashboard');
+let kitchen = document.querySelector('.kitchen');
+let hall = document.querySelector('.hall');
+
 console.dir(dashboard);
+
+function clearActive() {
+    let content = document.querySelector('.content');
+    let contentItem = content.querySelector('.active');
+    console.log(contentItem);
+    if (contentItem !== null) {
+        contentItem.classList.remove('active');
+    }
+
+}
 dashboard.addEventListener('click', () => {
+    clearActive();
     let imgDash = document.querySelector('.indicators');
     imgDash.classList.add('active');
 
-})
+});
+
+kitchen.addEventListener('click', () => {
+    console.log('www');
+    // kitchen.preventDefault();
+    clearActive();
+    let imgDash = document.querySelector('.kitchen-devices');
+    imgDash.classList.add('active');
+
+});
