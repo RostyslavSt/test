970dfe1e36f4150c32c0eb794f513aad6a216348
AnastasiiaHamaiunova <asikalove@yandex.ua>
2017-07-04 10:45:23 +0300

    style for control panel(search, filter, add)

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,11 +3,9 @@ require('./search.scss');
 
 const Search = (props) => {
   return (
-    <div className='searchBox'>
-      <input type="text"
-        onChange={e => {props.handleSearch(e.target.value);}}
-        placeholder='Search'/>
-      <div className='search-icon'><i className="fa fa-search"></i></div>
+    <div className='searchBox'>      
+      <input type="text" name="search" placeholder="Search" onChange={e =>
+            {props.handleSearch(e.target.value);}}/>
     </div>
   );
 };
