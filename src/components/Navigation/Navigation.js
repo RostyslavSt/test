47a75629695aacd5a927c6914672c83a7e265aaa
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-06 16:07:06 +0300

    added nav teg

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,13 +6,13 @@ import './Navigation.scss';
 export default class Navigation extends Component {
   render () {
     return (
-      // <div className="navigation_wrapper">
+      <nav>
         <ul className="navigation">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/builder">Device Builder</Link></li>
           <li><Link to="/devices">Device List</Link></li>
         </ul>
-      // </div>
+      </nav>
     );
   }
 }
