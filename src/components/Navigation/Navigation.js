a59978dfd6c1bbe573ae5594c816f3462390e5f5
Giperboloid <kostyamol@gmail.com>
2017-07-06 16:52:54 +0300

    fixed nav spaces

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,9 +8,27 @@ export default class Navigation extends Component {
     return (
       <nav>
         <ul className="navigation">
-          <li className="navigation-item"><NavLink to="/" activeClassName="active">Home</NavLink></li>
-          <li className="navigation-item"><NavLink to="/builder" activeClassName="active">Device Builder</NavLink></li>
-          <li className="navigation-item"><NavLink to="/devices" activeClassName="active">Device List</NavLink></li>
+          <li className="navigation-item">
+            <NavLink
+              to="/"
+              activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="navigation-item">
+            <NavLink
+             to="/builder"
+             activeClassName="active">
+             Device Builder
+            </NavLink>
+          </li>
+          <li className="navigation-item">
+            <NavLink
+             to="/devices"
+             activeClassName="active">
+             Device List
+            </NavLink>
+          </li>
         </ul>
       </nav>
     );
