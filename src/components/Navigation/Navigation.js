5cb52578f42907f8b20d683cf7d8fd8bfbbb6780
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-07-06 17:39:51 +0300

    change nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,34 +3,33 @@ import { NavLink } from 'react-router-dom';
 
 import './Navigation.scss';
 
-export default class Navigation extends Component {
-  render () {
-    return (
-      <nav>
-        <ul className="navigation">
-          <li className="navigation-item">
-            <NavLink
-              to="/"
-              activeClassName="active">
-              Home
-            </NavLink>
-          </li>
-          <li className="navigation-item">
-            <NavLink
-             to="/builder"
-             activeClassName="active">
-             Device Builder
-            </NavLink>
-          </li>
-          <li className="navigation-item">
-            <NavLink
-             to="/devices"
-             activeClassName="active">
-             Device List
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const Navigation = () => (
+  <nav>
+    <ul className="navigation">
+      <li className="navigation-item">
+        <NavLink
+          to="/"
+          activeClassName="active">
+          Home
+        </NavLink>
+      </li>
+      <li className="navigation-item">
+        <NavLink
+          to="/builder"
+          activeClassName="active">
+          Device Builder
+        </NavLink>
+      </li>
+      <li className="navigation-item">
+        <NavLink
+          to="/devices"
+          activeClassName="active">
+          Device List
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
+
