753a11b3f30601d9272cebaa6cfd6f830d733d18
Kate <abramtsova.k@gmail.com>
2017-07-07 19:54:42 +0300

    fixed some scss

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,8 +4,7 @@ import { NavLink } from 'react-router-dom';
 import './Navigation.scss';
 
 const Navigation = () => (
-  <nav className='mob-hide'>
-    <ul className="navigation">
+    <ul className="navigation mob-hide">
       <li className="navigation-item">
         <NavLink
           to="/"
@@ -28,7 +27,6 @@ const Navigation = () => (
         </NavLink>
       </li>
     </ul>
-  </nav>
 );
 
 export default Navigation;
