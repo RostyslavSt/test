a310e792e20d9f7152f42ca1baba6c0b27b5e7b6
Giperboloid <kostyamol@gmail.com>
2017-07-07 16:18:20 +0300

    fixed nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,31 +3,29 @@ import { NavLink } from 'react-router-dom';
 import './Navigation.scss';
 
 const Navigation = () => (
-  <nav>
-    <ul className="navigation">
-      <li className="navigation-item">
-        <NavLink
-          to="/"
-          exact activeClassName="active">
-          Home
-        </NavLink>
-      </li>
-      <li className="navigation-item">
-        <NavLink
-          to="/builder"
-          activeClassName="active">
-          Device Builder
-        </NavLink>
-      </li>
-      <li className="navigation-item">
-        <NavLink
-          to="/devices"
-          activeClassName="active">
-          Device List
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
+  <ul className="navigation">
+    <li className="navigation-item">
+      <NavLink
+        to="/"
+        exact activeClassName="active">
+        Home
+      </NavLink>
+    </li>
+    <li className="navigation-item">
+      <NavLink
+        to="/builder"
+        activeClassName="active">
+        Device Builder
+      </NavLink>
+    </li>
+    <li className="navigation-item">
+      <NavLink
+        to="/devices"
+        activeClassName="active">
+        Device List
+      </NavLink>
+    </li>
+  </ul>
 );
 
 export default Navigation;
