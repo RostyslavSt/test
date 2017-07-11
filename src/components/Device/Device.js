6e8e6caf84ed9445dd15ab49034c81ec859fe8ef
Yekaterina <abramtsova.k@gmail.com>
2017-07-11 07:47:30 +0300

    fixed problem with babel-polyfill

---
+++ src/components/Device/Device.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { setingsComponents } from '../../data/componentsNames';
+import PropTypes from 'prop-types';
+
+export const Device = (props) => {
+  const device = props.device;
+  const onStatusChange = props.onStatusChange;
+
+  return (
+    <seciton className="device-view">
+      <div className="device-view__header">
+        <div className="device-view__name">
+          <h3>{device.name}</h3>
+          <small>Last updated 7 days ago</small>
+        </div>
+        <div className="device-item__info-status">
+          <label className="switch">
+            <input
+              type="checkbox"
+              onChange={onStatusChange}
+              checked={device.status}
+            />
+            <div className="slider round"></div>
+          </label>
+        </div>
+        <div className="device-view__creation-info">
+          <small>07.06.2017</small><br/>
+          <small>User</small>
+        </div>
+      </div>
+      <div className="device-view__location">
+        <h4>
+          <i className="fa fa-map-marker"></i>{device.location}
+        </h4>
+      </div>
+      <section className="device-view__settings">
+        {device.item.map((setting, i) => {
+          const SettingsComponent = setingsComponents[setting.name];
+
+          return (
+            <SettingsComponent
+            key={'setting' + i}
+            styleName={
+              'device-view__settings-' + setting.name.toLowerCase()
+            }/>);
+        })}
+      </section>
+    </seciton>
+  );
+};
+
+Device.propTypes = {
+  device: PropTypes.object.isRequired,
+  onStatusChange: PropTypes.func
+};
--- src/pages/DevicePage/DevicePage.js
+++ src/pages/DevicePage/DevicePage.js
@@ -1,63 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import DeviceList from '../DeviceList/DeviceList';
-import { setingsComponents } from '../../data/componentsNames';
-import LineChart from '../../components/LineChart/LineChart';
+import { Device } from '../../components/Device/Device';
 import PropTypes from 'prop-types';
 import { loadDeviceAsync } from '../../actions/loadDevices.action';
 require('./DevicePage.scss');
-
-const Device = (props) => {
-  const device = props.device;
-  const onStatusChange = props.onStatusChange;
-
-  return (
-    <seciton className="device-view">
-      <div className="device-view__header">
-        <div className="device-view__name">
-          <h3>{device.name}</h3>
-          <small>Last updated 7 days ago</small>
-        </div>
-        <div className="device-item__info-status">
-          <label className="switch">
-            <input
-              type="checkbox"
-              onChange={onStatusChange}
-              checked={device.status}
-            />
-            <div className="slider round"></div>
-          </label>
-        </div>
-        <div className="device-view__creation-info">
-          <small>07.06.2017</small><br/>
-          <small>User</small>
-        </div>
-      </div>
-      <div className="device-view__location">
-        <h4>
-          <i className="fa fa-map-marker"></i>{device.location}
-        </h4>
-      </div>
-      <section className="device-view__settings">
-        {device.item.map((setting, i) => {
-          const SettingsComponent = setingsComponents[setting.name];
-
-          return (
-            <SettingsComponent
-            key={'setting' + i}
-            styleName={
-              'device-view__settings-' + setting.name.toLowerCase()
-            }/>);
-        })}
-      </section>
-    </seciton>
-  );
-};
-
-Device.propTypes = {
-  device: PropTypes.object.isRequired,
-  onStatusChange: PropTypes.func
-};
 
 class DevicePage extends React.Component {
   constructor (props) {
@@ -98,6 +44,5 @@ DevicePage.propTypes = {
   findItems: PropTypes.func,
   loadDevice: PropTypes.func
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(DevicePage);
