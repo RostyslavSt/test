a7532db446f9fb7d87bf66c4ac053fa598df4f7e
rost <rost@ukr.net>
2017-07-28 14:34:17 +0300

    added server

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 
-const ws = new WebSocket('ws://localhost:3000');
+const ws = new WebSocket('ws://localhost:3003');
 function setStatus(value) {
     status.innerHTML = value;
 }
@@ -15,7 +15,11 @@ function printMessage(value) {
     messages.appendChild(li);
 }
 
-ws.onopen = () => setStatus('OMLONE');
+ws.onopen = () => {
+    setStatus('ONLINE');
+    console.log('d232323d');
+}
+// ws.onopen = () => console.log('dwdwd');
 
 ws.onclose = () => setStatus('disconnected');
 
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 3003});
 
 server.on('connection', ws => {
-    ws.send('welcome')
+    ws.send('welcome888999')
 })
