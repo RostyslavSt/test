3ab302dfcaf7320416336409db76adca041d754a
rostyslav.shtets <rostyslav.shtets@sigma.software>
2019-02-08 16:58:03 +0200

    cron and logrotate are removed from this branch

--- cron/script_for_seconds.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-I=0
-
-while [ ${I} -le 50 ]; do
-I=`/usr/bin/expr ${I} + 15`
-/home/rshtets/devOps_projects/study_tasks/cron/test_script.sh
-sleep 15
-done
--- cron/test_script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-echo "Hello, world!"
-touch /home/rshtets/test2019/test_$(date +'%Y%m%d-%H%M%S')
